d3ee3f7d53bb798e692aab3b604b8f2922975fdf
Anna Mityaeva <64027105+Anna-the-racoon@users.noreply.github.com>
2021-11-27 02:43:28 +0300

    Created Service, updated Client

---
+++ Client/Models/Security/AuthorizationVm.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Client.Models.Security
+{
+    public class AuthorizationVm
+    {
+        public string Name { get; set; }
+    }
+}
